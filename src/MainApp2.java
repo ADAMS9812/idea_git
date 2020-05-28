bd35b3ca3ea7366d823926104c2379e71320430e
yangshiwen <yangshiwen@xinli001.com>
2020-05-28 16:04:43 +0800

    test demo

--- src/MainApp2.java
+++
@@ -1,9 +0,0 @@
-public class MainApp2 {
-
-    public static void main(String[] args) {
-
-        System.out.println("这里是第三次使用IDEA提交");
-
-    }
-
-}
--- src/MainApp3.java
+++
@@ -1,9 +0,0 @@
-public class MainApp3 {
-
-    public static void main(String[] args) {
-
-        System.out.println("跑起来");
-
-    }
-
-}
