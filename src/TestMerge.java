cbd2ef4e5bebbfe2c40d7b8e71dd520d813ffd27
yangshiwen <yangshiwen@xinli001.com>
2020-05-28 17:41:41 +0800

    new test !!!!

--- src/TestMerge.java
+++ src/TestMerge.java
@@ -7,6 +7,9 @@ public class TestMerge {
         System.out.println("测试merge合并分支用的");
 
         System.out.println("here is new test");
+        System.out.println("here is new test");
+        System.out.println("here is new test");
+        System.out.println("here is new test");
 
     }
 
