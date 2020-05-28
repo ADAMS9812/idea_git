37c5ab121bc07ce40bfa118ac225ef065818fdb5
yangshiwen <yangshiwen@xinli001.com>
2020-05-28 16:39:13 +0800

    测试合并分支2222

--- src/TestMerge.java
+++ src/TestMerge.java
@@ -3,6 +3,8 @@ public class TestMerge {
     public static void main(String[] args) {
 
         System.out.println("测试merge合并分支用的");
+        System.out.println("测试merge合并分支用的");
+        System.out.println("测试merge合并分支用的");
 
     }
 
