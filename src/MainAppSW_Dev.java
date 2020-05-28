bbfc7234c25e358246a00671875476537430cdac
yangshiwen <yangshiwen@xinli001.com>
2020-05-28 15:58:39 +0800

    sw_dev 22222

--- src/MainAppSW_Dev.java
+++ src/MainAppSW_Dev.java
@@ -3,6 +3,8 @@ public class MainAppSW_Dev {
     public static void main(String[] args) {
 
         System.out.println("这里sw dev");
+        System.out.println("这里sw dev");
+        System.out.println("这里sw dev");
 
     }
 
